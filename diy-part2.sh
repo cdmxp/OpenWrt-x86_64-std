3c44347117b186ae35fba6d45d90dbe55daa4ed3
cdmxp <58542703+cdmxp@users.noreply.github.com>
2022-10-26 23:44:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改openwrt登陆地址
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+# 修改概览里时间显示为中文数字
+# sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/x86/index.htm
