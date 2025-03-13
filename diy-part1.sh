29e231ed6c9a70bb28f54dfe2cfb85ffa95fd3d5
cdmxp <58542703+cdmxp@users.noreply.github.com>
2025-03-13 17:45:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac;v1.0' >>feeds.conf.default
